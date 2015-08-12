317e694c7551671746763292897c689e5597e383
Eugene Sharygin <eush77@gmail.com>
2015-08-13 00:07:26 +0300

    More tests, mock network requests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,24 @@
 'use strict';
 
-var githubReadme = require('..');
+var rewire = require('rewire');
+
+var githubReadme = rewire('..');
 
 var test = require('tape');
+
+var fs = require('fs');
 
 
 test(function (t) {
   t.plan(2);
+
+  githubReadme.__set__('ghget', fakeGithubGet({
+    'LICENSE': '',
+    'main.c': '',
+    'CONTRIBUTING.md': '',
+    'README': fs.readFileSync(__dirname + '/git_readme.txt', 'utf8')
+  }));
+
   githubReadme('git', 'git', function (err, readme) {
     t.ifErr(err);
     t.true(readme.indexOf('the stupid content tracker') >= 0);
@@ -14,9 +26,50 @@ test(function (t) {
 });
 
 
-test('non-existent', function (t) {
+test('non-existent repo', function (t) {
   t.plan(1);
+
+  githubReadme.__set__('ghget', fakeGithubGet());
+
   githubReadme('git', Math.random().toString(36).slice(2), function (err) {
     t.true(err);
   });
 });
+
+
+test('non-existent file', function (t) {
+  t.plan(1);
+
+  githubReadme.__set__('ghget', fakeGithubGet({
+    'LICENSE': '',
+    'main.c': '',
+    'CONTRIBUTING.md': ''
+  }));
+
+  githubReadme('git', 'git', function (err) {
+    t.true(err);
+  });
+});
+
+
+function fakeGithubGet (files) {
+  return function (user, repo, path, opts, cb) {
+    if (typeof path != 'string') {
+      cb = opts;
+      opts = path;
+      path = '/';
+    }
+    if (typeof opts == 'function') {
+      cb = opts;
+      opts = {};
+    }
+
+    return (path == '/'
+            ? (files
+               ? cb(null, null, Object.keys(files))
+               : cb(Error('repo does not exist')))
+            : (files && files[path]
+               ? cb(null, null, files[path])
+               : cb(Error('file does not exist'))));
+  };
+}
